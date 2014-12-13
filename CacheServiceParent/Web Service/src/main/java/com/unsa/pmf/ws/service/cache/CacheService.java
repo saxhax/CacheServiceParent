74e388b50f7d78b1a6b0ad903fb203441e1f4291
Akif Hadziabdic <akedxs@gmail.com>
2014-12-13 23:22:37 +0100

    feature web service deployed on jboss correctly

--- CacheServiceParent/Web Service/src/main/java/com/unsa/pmf/ws/service/cache/CacheService.java
+++ CacheServiceParent/Web Service/src/main/java/com/unsa/pmf/ws/service/cache/CacheService.java
@@ -2,18 +2,21 @@ package com.unsa.pmf.ws.service.cache;
 
 import com.unsa.pmf.ws.service.Service;
 import com.unsa.pmf.ws.service.Session;
+import com.unsa.pmf.ws.service.cache.impl.SessionImpl;
 
+import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
-public interface CacheService extends Service{
+public interface CacheService{
 
 	/**
 	 * Check is service started
 	 * @param serviceName
 	 * @return
 	 */
+	@WebMethod
 	public boolean isServiceStarted(@WebParam(name = "serviceName")String serviceName);
 
 	/**
@@ -22,5 +25,6 @@ public interface CacheService extends Service{
 	 * @return
 	 * 		Service session
 	 */
-	public Session startService(@WebParam(name = "serviceName")String serviceName);
+	@WebMethod
+	public SessionImpl startService(@WebParam(name = "serviceName")String serviceName);
 }
